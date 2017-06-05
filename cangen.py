e47a7e37f8a665e07463daf86fa0aef017f2d861
Paul <paulerikf@gmail.com>
2017-06-05 17:52:37 +0200

    Change cangen stuff

--- cangen.py
+++ cangen.py
@@ -5,18 +5,12 @@ import math
 
 import struct
 
-bus = can.interface.Bus(channel='vcan0', bustype='socketcan_native')
-i = 0
+bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
 
 
 def send():
-    global i
-    i += 0.5
-    v = int(40 * math.sin(i) + 50)
-    print(v)
-    m = [n for n in struct.pack('I', v)]
-    msg = can.Message(arbitration_id=0x7b,
-                      data=m,
+    msg = can.Message(arbitration_id=0x4C0,
+                      data=8,
                       extended_id=False)
     bus.send(msg)
     sleep(0.001)
